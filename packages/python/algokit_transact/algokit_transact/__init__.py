39c6c606a3744ea0b0988ba9d0b1ccad611b2686
Joe Polny <joepolny@gmail.com>
2025-06-13 13:45:04 -0400

    build(python): use poetry build system instead of maturin

--- packages/python/algokit_transact/algokit_transact/__init__.py
+++ packages/python/algokit_transact/algokit_transact/__init__.py
@@ -3,6 +3,6 @@ AlgoKit Kit Transaction Library Python Bindings
 """
 
 # Import all symbols from the Rust extension module and re-export them
-from _algokit_transact import *
+from .algokit_transact_ffi import *
 
 # Add any additional exports or initialization here
--- tools/build_pkgs/src/python.rs
+++ tools/build_pkgs/src/python.rs
@@ -1,14 +1,70 @@
-use std::path::PathBuf;
-
-use crate::{Package, run};
-use color_eyre::eyre::Result;
+use crate::{Package, get_repo_root, run};
+use color_eyre::eyre::{Result, eyre};
 
 pub fn build(package: &Package) -> Result<()> {
+    let crate_name = package.crate_name();
+    let ext = if cfg!(target_os = "windows") {
+        "dll"
+    } else if cfg!(target_os = "macos") {
+        "dylib"
+    } else {
+        "so"
+    };
+
     run(
-        "maturin build",
-        Some(&PathBuf::from(&format!("packages/python/{package}"))),
+        &format!(
+            r#"cargo --color always build --release --manifest-path "{}""#,
+            package.crate_manifest().display()
+        ),
+        None,
         None,
     )?;
+
+    let package_dir = get_repo_root()
+        .join("packages")
+        .join("python")
+        .join(package.to_string());
+    let module_dir = package_dir.join(package.to_string());
+
+    run(
+        &format!(
+            r#"cargo --color always run -p uniffi-bindgen generate --no-format --library "{}" --language python --out-dir "{}""#,
+            package.dylib().display(),
+            module_dir.display()
+        ),
+        None,
+        None,
+    )?;
+
+    std::fs::copy(
+        package.dylib(),
+        module_dir.join(package.dylib().file_name().unwrap()),
+    )?;
+
+    run("poetry install --only build", Some(&package_dir), None)?;
+
+    run("poetry build --format wheel", Some(&package_dir), None)?;
+
+    if cfg!(target_os = "linux") {
+        let dist_files: Vec<_> = std::fs::read_dir(package_dir.join("dist"))?
+            .filter_map(Result::ok)
+            .filter(|entry| entry.path().extension().map_or(false, |ext| ext == "whl"))
+            .collect();
+
+        if dist_files.is_empty() {
+            return Err(eyre!("No .whl files found in dist directory"));
+        } else if dist_files.len() > 1 {
+            return Err(eyre!("Multiple .whl files found in dist directory"));
+        }
+
+        let wheel_file = dist_files[0].path();
+
+        run(
+            &format!(r#"poetry run auditwheel repair "{}""#, wheel_file.display()),
+            Some(&package_dir),
+            None,
+        )?;
+    }
 
     Ok(())
 }
