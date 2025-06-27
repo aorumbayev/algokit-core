6f337f48d2b0206eb65a08efe587e72ace9e299f
Hoang Dinh <patrick.dinh@makerx.com.au>
2025-06-27 13:03:12 +1000

    chore: delete API clients generated by mustache templates

--- packages/python/algod_api/release.config.cjs
+++
@@ -1,8 +0,0 @@
-const releaseUtils = require("../../../utils/semantic-release.cjs");
-
-module.exports = releaseUtils.getConfig({
-  language: "python",
-  packageName: "algod_api",
-  isNative: true,
-  assets: ["../../../artifacts/algokit_algod_api*.whl"],
-});
