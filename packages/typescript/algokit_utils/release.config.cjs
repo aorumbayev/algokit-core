33b1155ed881eddc0d3f6e317b4611d2ec97dcdd
Altynbek Orumbayev <altynbek.orumbayev@makerx.com.au>
2025-10-09 18:04:21 +0200

    chore: test

--- packages/typescript/algokit_utils/release.config.cjs
+++ packages/typescript/algokit_utils/release.config.cjs
@@ -7,6 +7,22 @@ const config = releaseUtils.getConfig({
   packageName: 'algokit_utils',
 })
 
+// Disable GitHub issue/PR commenting to avoid GraphQL rate limits
+config.plugins = config.plugins.map((plugin) => {
+  if (Array.isArray(plugin) && plugin[0] === '@semantic-release/github') {
+    return [
+      '@semantic-release/github',
+      {
+        ...plugin[1],
+        successComment: false,
+        failComment: false,
+        releasedLabels: false,
+      },
+    ]
+  }
+  return plugin
+})
+
 config.plugins = [...config.plugins, ['@semantic-release/npm', { npmPublish: true }]]
 
 module.exports = config
