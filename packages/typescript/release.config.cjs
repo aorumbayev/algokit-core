f101d25b02c833b9dfe261f0a4596ae4ce7b3bc1
Altynbek Orumbayev <altynbek.orumbayev@makerx.com.au>
2025-10-16 14:17:00 +0200

    chore: test

--- packages/typescript/release.config.cjs
+++ packages/typescript/release.config.cjs
@@ -10,12 +10,29 @@ const config = releaseUtils.getConfig({
   packageName,
 })
 
+// Disable GitHub issue/PR commenting to avoid GraphQL rate limits
+config.plugins = config.plugins.map((plugin) => {
+  if (Array.isArray(plugin) && plugin[0] === '@semantic-release/github') {
+    return [
+      '@semantic-release/github',
+      {
+        ...plugin[1],
+        successComment: false,
+        failComment: false,
+        releasedLabels: false,
+      },
+    ]
+  }
+  return plugin
+})
+
 config.plugins = [
   ...config.plugins,
   [
     '@semantic-release/npm',
     {
       npmPublish: true,
+      // Publish from the built package directory (relative to packages/typescript)
       pkgRoot: `${packageName}/dist`,
     },
   ],
